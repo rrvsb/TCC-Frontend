1a285468a5e06f492b49d3f6f91f3e9f14db9dec
ANDERSON LIMA DA SILVA <00001102357686sp@aluno.educacao.sp.gov.br>
2024-05-02 08:44:35 -0300

    login & register containers update

--- js/login/login-style-script.js
+++ js/login/login-style-script.js
@@ -1,5 +1,35 @@
 const title = document.querySelector(".title");
 const titlePage = document.title;
+const ContainerTitle = document.querySelector(".Container-user-title");
+
+const ContainerLink = document.querySelector(".Container-user-link");
+const ContainerLoginForm = document.querySelector("#formLogin");
+const ContainerRegisterForm = document.querySelector(".formRegister");
+
+const cadastroOuLogin = document.querySelector("#Container-user-link-loginRegister");
+const passwordForgot = document.querySelector("#Container-user-link-PasswordForgot");
+let registerOrLogin = false;
+
+ContainerLink.addEventListener("click", () => {
+    registerOrLogin = !registerOrLogin;
+    if (registerOrLogin) {
+        ContainerTitle.textContent = "cadastro"
+        ContainerLoginForm.style.display = "none"
+        ContainerRegisterForm.style.display = "block"
+        passwordForgot.style.display = "none"
+        cadastroOuLogin.textContent = "login";
+        registerOrLogin = true;
+    } 
+    if (!registerOrLogin) {
+        ContainerTitle.textContent = "Login"
+        ContainerLoginForm.style.display = "block"
+        ContainerRegisterForm.style.display = "none"
+        passwordForgot.style.display = "block"
+        cadastroOuLogin.textContent = "cadastrar-se";
+        registerOrLogin = false;
+    }
+})
+
 const renderTitle = () => {
     const titleArray = ["C", "o", "d", "e", "S", "a", "v", "v", "i", "y"];
     let index = 0;
@@ -10,7 +40,7 @@ const renderTitle = () => {
     // Set an interval to add each character
     const intervalId = setInterval(() => {
         if (index < titleArray.length) {
-            
+
             if (index === 8) {
                 title.textContent += titleArray[index];
                 setTimeout(() => {
