efe87ccc470fe01512df921616c8d5f8d3abd9bf
quantxz <indiedev.core@gmail.com>
2024-05-07 19:39:11 -0300

    messages from backend renderization system creation

--- js/chat/style.js
+++ js/chat/style.js
@@ -1,13 +1,31 @@
-const messageContent = document.querySelector('.message .content');
-const message = document.querySelector('.message');
+const contacts = document.querySelectorAll(".contact");
 
-messageContent.addEventListener('input', function() {
-    const lineHeight = parseInt(window.getComputedStyle(this).lineHeight);
-    const lines = Math.ceil(this.scrollHeight / lineHeight);
+contacts.forEach(item => {
+    item.id = ""
+    item.addEventListener("click", async (e) => {
+        if (item.id == "") {
+            const room = item.getAttribute("room");
+            item.id = "chatSelected";
+            removeSelectedFromOthers(item);
 
-    if (lines > 5) {
-        message.classList.add('expanded');
-    } else {
-        message.classList.remove('expanded');
-    }
-});
+            // Obtém o URL base sem os parâmetros de consulta
+            const baseUrl = window.location.origin + window.location.pathname;
+
+            // Cria um objeto URLSearchParams apenas com os novos parâmetros
+            let params = new URLSearchParams();
+            params.append("room", room);
+
+            // Atualiza a URL sem parâmetros de consulta e adiciona os novos parâmetros
+            window.history.replaceState({}, '', baseUrl + '?' + params.toString());
+        }
+
+    })
+})
+
+function removeSelectedFromOthers(selectedItem) {
+    contacts.forEach(item => {
+        if (item !== selectedItem && item.id === "chatSelected") {
+            item.id = "";
+        }
+    });
+}
